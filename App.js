5a99946961f70d494b3ead6817c907c0bbd577a8
System Administrator <melanie.terzic@gmail.com>
2020-04-03 10:57:54 +0200

    Add StoryBook

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +14,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
+
+async function loadResourcesAndDataAsync() {
+  try {
+    SplashScreen.preventAutoHide()
+
+    // Load our initial navigation state
+    setInitialNavigationState(await getInitialState())
+
+    // Load fonts
+    await Font.loadAsync({
+      ...Ionicons.font,
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      percolate: require('./assets/icon/percolate.ttf'),
+      'NunitoSans-Bold': require('./assets/font/NunitoSans-Bold.ttf'),
+      'NunitoSans-Italic': require('./assets/font/NunitoSans-Italic.ttf'),
+      NunitoSans: require('./assets/font/NunitoSans-Regular.ttf')
+    })
+  } catch (e) {
+    // We might want to provide this error information to an error reporting service
+    console.warn(e)
+  } finally {
+    setLoadingComplete(true)
+    SplashScreen.hide()
+  }
+}
