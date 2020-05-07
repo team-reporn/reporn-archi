25687303f9b6405d231507994a5b56d41bc57420
emile <emile31.roch@gmail.com>
2020-05-07 17:00:23 +0200

    Add connection with socket

--- src/App/Socket/index.js
+++ src/App/Socket/index.js
@@ -1,13 +1,16 @@
-import React, { useCallback, createContext } from 'react'
-
-export const Context = createContext(null)
+import React, { useCallback, createContext } from "react";
+import SocketIOClient from "socket.io-client";
+export const Context = createContext(null);
 
 const Socket = ({ children }) => {
+  const socket = SocketIOClient("http://192.168.1.37:4000"); // replace 'environment.serverUrl' with your server url
+
   const createRoom = useCallback(() => {
-    console.log('created room')
-  }, [])
+    socket.emit("channel1", "Hi server");
+    console.log("created room");
+  }, []);
 
-  return <Context.Provider value={{ createRoom }}>{children}</Context.Provider>
-}
+  return <Context.Provider value={{ createRoom }}>{children}</Context.Provider>;
+};
 
-export default Socket
+export default Socket;
