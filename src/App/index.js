95badebaf84b36514c8018d31bec26d3ba748066
emile <emile31.roch@gmail.com>
2020-05-18 17:04:07 +0200

    navigation new version

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Routes from "./routes";
-import Socket from "./Socket";
 import Home from "../pages/Home";
 
 export default class Index extends React.Component {
--- src/App/routes.js
+++ src/App/routes.js
@@ -1,44 +1,53 @@
-import { createStackNavigator, createAppContainer } from '@react-navigation/native'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Home from '../pages/Home'
-import SelectGame from '../pages/SelectGame'
-import Game from '../pages/Game'
-import EndGame from '../pages/EndGame'
+import Socket from "./Socket";
 
-export default class Routes extends React.Component {
-  render() {
-    return <AppContainer />
-  }
-}
+import Home from "../pages/Home";
+import SelectGame from "../pages/SelectGame";
+import Game from "../pages/Game";
+import EndGame from "../pages/EndGame";
 
-const AppNavigator = createStackNavigator(
-  {
-    //in this, create the home page -choose between create a room or PornNews- + Go to SelectGame
-    Home: {
-      screen: Home,
-      navigationOptions: {
-        headerShown: false,
-      },
-    },
-    // in this, define all games and select one randomly - On button to Game
-    SelectGame: {
-      screen: SelectGame,
-      headerShown: false,
-    },
-    // In this, see the question of the quiz if you select answer go to EndGame
-    Game: {
-      screen: Game,
-      headerShown: false,
-    },
-    // in this, show the answer
-    EndGame: {
-      screen: EndGame,
-      headerShown: false,
-    },
-  },
-  {
-    initialRouteName: 'Home',
-  }
-)
+const Stack = createStackNavigator();
 
-const AppContainer = createAppContainer(AppNavigator)
+export default () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={() => (
+            <Socket>
+              <Home />
+            </Socket>
+          )}
+        />
+        <Stack.Screen
+          name="SelectGame"
+          component={() => (
+            <Socket>
+              <SelectGame />
+            </Socket>
+          )}
+        />
+        <Stack.Screen
+          name="Game"
+          component={() => (
+            <Socket>
+              <Game />
+            </Socket>
+          )}
+        />
+        <Stack.Screen
+          name="EndGame"
+          component={() => (
+            <Socket>
+              <EndGame />
+            </Socket>
+          )}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
