3b3e922c3932dd473c84d5713e9fdbbdb9c825fd
Mélanie Gaubert <melanie.terzic@gmail.com>
2020-06-25 11:02:39 +0200

    add EndGame

--- src/pages/EndGame/index.js
+++ src/pages/EndGame/index.js
@@ -1,26 +1,91 @@
-import React from "react";
-import { Button, ButtonContainer } from "../../components/Button";
-import { View, StyleSheet, Text, Image } from "react-native";
+import React from 'react'
+import { Button, ButtonContainer } from '../../components/Button'
+import { View, StyleSheet, Text, Image } from 'react-native'
 
-import useSocket from "../../App/Socket/useSocket";
-import culture from "../../contents/cultureData";
-// import troudumonde from '../../contents/troudumonde'
-// import parodie from '../../contents/parodie'
+import useSocket from '../../App/Socket/useSocket'
+
+import TitleAnswer from '../../components/titles/TitleAnswer'
+import TitleAnswer2 from '../../components/titles/TitleAnswer2'
+import Answer from '../../components/Paragraph/Answer'
+import PornnewsFlash from '../../components/Paragraph/pornnewsFlash'
+import NextButton from '../../components/btn/NextBtn.js'
+import SelectGame from '../SelectGame'
+
+import content from './content'
+
+const questions = content
+let gameIndex = 0
+
+let getAnswerfromGame = ({ game }) => {
+  switch (game) {
+    case 'tabou':
+      return 4
+      break
+
+    case 'cultureQ':
+      return 0
+      break
+
+    case 'acteurX':
+      return 2
+      break
+
+    case 'ouEst':
+      return 1
+      break
+
+    default:
+      throw 'non reconized game ' + game
+      break
+  }
+}
 
 export default ({ navigation }) => {
-  const { changeGame, game } = useSocket();
+  const { game } = useSocket()
+  gameIndex = getAnswerfromGame(game)
   return (
     <View>
-      <Text>this was the laps : {game.round.laps}</Text>
+      <Image
+        source={require('../../assets/img/pictos/Bon.png')}
+        style={styles.image}
+      />
+      <TitleAnswer content="BIeN JOuÉ !" />
+      <TitleAnswer2
+        content={questions[gameIndex].content}
+        content2={questions[gameIndex].content2}
+      />
+      <Answer content={questions[gameIndex].paragraph} />
+      <PornnewsFlash content={questions[gameIndex].pornews}></PornnewsFlash>
+      <SelectGame navigation={navigation} />
+      {/* <NextButton
+        title="suite"
+        text="suite"
+        onPress={() => {
+          navigation.navigate('SelectGame', {
+            title: 'SelectGame',
+          })
+        }}
+      /> */}
+      {/* <ButtonContainer>
+        <BigButton
+          key="end game"
+          text="end game"
+          onPress={() => {
+            navigation.navigate('EndGame', {
+              title: 'EndGame',
+            })
+          }}
+        />
+      </ButtonContainer>
       {game.round.laps < 4 ? (
         <Button
           key="QUESTION"
           text="QUESTION"
           onPress={() => {
-            changeGame();
-            navigation.navigate("SelectGame", {
-              title: "SelectGame",
-            });
+            changeGame()
+            navigation.navigate('SelectGame', {
+              title: 'SelectGame',
+            })
           }}
         />
       ) : (
@@ -28,13 +93,26 @@ export default ({ navigation }) => {
           key="Shake"
           text="Shake"
           onPress={() => {
-            changeGame();
-            navigation.navigate("Shake", {
-              title: "Shake",
-            });
+            changeGame()
+            navigation.navigate('Shake', {
+              title: 'Shake',
+            })
           }}
         />
-      )}
+      )} */}
     </View>
-  );
-};
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '30%',
+  },
+  image: {
+    marginBottom: 30,
+    width: 64,
+    height: 64,
+  },
+})
--- src/pages/Game/ActeurX/index.js
+++ src/pages/Game/ActeurX/index.js
@@ -1,68 +1,46 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, ImageBackground } from 'react-native'
-import useSocket from '../../../App/Socket/useSocket'
+// import React, { useState } from 'react'
+// import { View, StyleSheet, ImageBackground } from 'react-native'
+// import useSocket from '../../App/Socket/useSocket'
 
-import Title1 from '../../../components/titles/Title2'
-import QuizzBtn from '../../../components/btn/QuizzBtn'
+// import Title1 from '../../components/titles/Title2'
+// import QuizzBtn from '../../components/btn/QuizzBtn'
 
-import acteurX from '../../../contents/acteurX'
+// import acteurX from '../../contents/acteurX'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
-  background: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'column',
+//   },
+//   background: {
+//     flex: 1,
+//     resizeMode: 'cover',
+//     justifyContent: 'center',
+//   },
+// })
 
-const Quiz = ({ navigation }) => {
-  const [correctCount, setCorrectCount] = useState(0)
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
-  const [answered, setAnswered] = useState(false)
-  const [answerCorrect, setAnswerCorrect] = useState(false)
+// const Quiz = ({ navigation }) => {
+//   const [correctCount, setCorrectCount] = useState(0)
+//   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
+//   const [answered, setAnswered] = useState(false)
+//   const [answerCorrect, setAnswerCorrect] = useState(false)
 
-  answer = (correct) => {
-    setAnswered(true)
-    if (correct) {
-      setCorrectCount(correctCount + 1)
-      setAnswerCorrect(true)
-    } else {
-      setAnswerCorrect(false)
-    }
-  }
+//   answer = (correct) => {
+//     setAnswered(true)
+//     if (correct) {
+//       setCorrectCount(correctCount + 1)
+//       setAnswerCorrect(true)
+//     } else {
+//       setAnswerCorrect(false)
+//     }
+//   }
 
-  const { game } = useSocket()
+//   const { game } = useSocket()
 
-  const questions = acteurX
-  const question = questions[activeQuestionIndex]
+//   const questions = acteurX
+//   const question = questions[activeQuestionIndex]
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require('../../../assets/img/backgrounds/Background3.png')}
-        style={styles.background}
-      >
-        <Title1 content={question.question} style={styles.title} />
-        {question.answers.map((answer) => (
-          <QuizzBtn
-            key={answer.id}
-            content={answer.text}
-            style={styles.text}
-            rotation
-            onPress={() =>
-              navigation.navigate('EndGame', {
-                title: 'EndGame',
-              })
-            }
-          />
-        ))}
-      </ImageBackground>
-    </View>
-  )
-}
+//   return <View style={styles.container}></View>
+// }
 
-export default Quiz
+// export default Quiz
