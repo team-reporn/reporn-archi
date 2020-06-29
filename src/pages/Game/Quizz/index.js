f6d7e2fde749b6c04e96c387ca31ec139d654f9b
emile <emile31.roch@gmail.com>
2020-06-29 09:59:05 +0200

    Add chrono on quizz

--- src/pages/Game/Quizz/index.js
+++ src/pages/Game/Quizz/index.js
@@ -1,37 +1,47 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, Text, Image } from 'react-native'
-import useSocket from '../../../App/Socket/useSocket'
+import React, { useState } from "react";
+import { View, StyleSheet, Text, Image } from "react-native";
+import useSocket from "../../../App/Socket/useSocket";
 
-import TitleQuestion from '../../../components/titles/TitleQuestion'
-import MainBtn from '../../../components/btn/MainBtn'
+import TitleQuestion from "../../../components/titles/TitleQuestion";
+import MainBtn from "../../../components/btn/MainBtn";
 
-import culture from '../../../contents/cultureData'
+import culture from "../../../contents/cultureData";
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: '30%',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "30%",
   },
   buttons: {
     marginTop: 50,
   },
-})
+});
 
 const Quiz = ({ navigation }) => {
-  const [correctCount, setCorrectCount] = useState(0)
-  const [answered, setAnswered] = useState(false)
-  const [answerCorrect, setAnswerCorrect] = useState(false)
-  const [step, setStep] = useState(0)
-  let index = 0
+  const [correctCount, setCorrectCount] = useState(0);
+  const [answered, setAnswered] = useState(false);
+  const [answerCorrect, setAnswerCorrect] = useState(false);
+  const [step, setStep] = useState(0);
+  let index = 0;
 
-  const { game, setSuccess, setQuestionIndex, QuestionIndex } = useSocket()
+  const { game, setSuccess, setQuestionIndex, QuestionIndex } = useSocket();
 
-  const questions = culture
-  const question = questions[QuestionIndex ? (index = 1) : (index = 0)]
+  const questions = culture;
+  const question = questions[QuestionIndex ? (index = 1) : (index = 0)];
 
   return (
     <View style={styles.container}>
+      <Chrono
+        duration={30}
+        onFinish={() => {
+          setStep(1);
+          setWin(false);
+          navigation.navigate("EndGame", {
+            title: "EndGame",
+          });
+        }}
+      />
       <TitleQuestion taille1 content={question.question} />
       <View style={styles.buttons}>
         <MainBtn
@@ -39,11 +49,11 @@ const Quiz = ({ navigation }) => {
           style={styles.text}
           rotation1
           onPress={() => {
-            setQuestionIndex(true)
-            setSuccess(question.answers[0].correct)
-            navigation.navigate('EndGame', {
-              title: 'EndGame',
-            })
+            setQuestionIndex(true);
+            setSuccess(question.answers[0].correct);
+            navigation.navigate("EndGame", {
+              title: "EndGame",
+            });
           }}
         />
         <MainBtn
@@ -51,11 +61,11 @@ const Quiz = ({ navigation }) => {
           style={styles.text}
           rotation2
           onPress={() => {
-            setQuestionIndex(true)
-            setSuccess(question.answers[1].correct)
-            navigation.navigate('EndGame', {
-              title: 'EndGame',
-            })
+            setQuestionIndex(true);
+            setSuccess(question.answers[1].correct);
+            navigation.navigate("EndGame", {
+              title: "EndGame",
+            });
           }}
         />
         <MainBtn
@@ -63,11 +73,11 @@ const Quiz = ({ navigation }) => {
           style={styles.text}
           rotation3
           onPress={() => {
-            setQuestionIndex(true)
-            setSuccess(question.answers[2].correct)
-            navigation.navigate('EndGame', {
-              title: 'EndGame',
-            })
+            setQuestionIndex(true);
+            setSuccess(question.answers[2].correct);
+            navigation.navigate("EndGame", {
+              title: "EndGame",
+            });
           }}
         />
         <MainBtn
@@ -75,16 +85,16 @@ const Quiz = ({ navigation }) => {
           style={styles.text}
           rotation4
           onPress={() => {
-            setQuestionIndex(true)
-            setSuccess(question.answers[3].correct)
-            navigation.navigate('EndGame', {
-              title: 'EndGame',
-            })
+            setQuestionIndex(true);
+            setSuccess(question.answers[3].correct);
+            navigation.navigate("EndGame", {
+              title: "EndGame",
+            });
           }}
         />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default Quiz
+export default Quiz;
